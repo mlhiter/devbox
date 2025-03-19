add1b115362d51031d56442ee795c59258fd21ae
mlhiter <3076438032@qq.com>
2025-03-19 11:58:40 +0800

    feat(webview): welcome

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,34 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEvent } from 'react-use'
+import { useCallback, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import WelcomeView from './components/welcome/WelcomeView'
+import { ExtensionMessage } from '../../src/shared/ExtensionMessage'
+import { ExtensionStateContextProvider, useExtensionState } from './context/ExtensionStateContext'
+
+const AppContent = () => {
+  const { didHydrateState, showWelcome, shouldShowAnnouncement, vscMachineId } = useExtensionState()
+  const [showSettings, setShowSettings] = useState(false)
+  const [showHistory, setShowHistory] = useState(false)
+  const [showMcp, setShowMcp] = useState(false)
+  const [showAccount, setShowAccount] = useState(false)
+  const [showAnnouncement, setShowAnnouncement] = useState(false)
+
+  const handleMessage = useCallback((e: MessageEvent) => {
+    const message: ExtensionMessage = e.data
+    switch (message.type) {
+      case 'action':
+        switch (message.action!) {
+          case 'settingsButtonClicked':
+            setShowSettings(true)
+            setShowHistory(false)
+            setShowMcp(false)
+            setShowAccount(false)
+            break
+          case 'historyButtonClicked':
+            setShowSettings(false)
+            setShowHistory(true)
+            setShowMcp(false)
+            setShowAccount(false)
+            break
+          case 'mcpButtonClicked':
+            setShowSettings(false)
+            setShowHistory(false)
+            setShowMcp(true)
+            setShowAccount(false)
+            break
+          case 'accountLoginClicked':
+            setShowSettings(false)
+            setShowHistory(false)
+            setShowMcp(false)
+            setShowAccount(true)
+            break
+          case 'chatButtonClicked':
+            setShowSettings(false)
+            setShowHistory(false)
+            setShowMcp(false)
+            setShowAccount(false)
+            break
+        }
+        break
+    }
+  }, [])
+
+  useEvent('message', handleMessage)
+
+  // useEffect(() => {
+  //   if (shouldShowAnnouncement) {
+  //     setShowAnnouncement(true)
+  //     vscode.postMessage({ type: 'didShowAnnouncement' })
+  //   }
+  // }, [shouldShowAnnouncement])
+
+  // if (!didHydrateState) {
+  //   return null
+  // }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <WelcomeView />
+    // <>
+    //   {showWelcome ? (
+    //     <WelcomeView />
+    //   ) : (
+    //     <>
+    //       {showSettings && (
+    //         <SettingsView onDone={() => setShowSettings(false)} />
+    //       )}
+    //       {showHistory && <HistoryView onDone={() => setShowHistory(false)} />}
+    //       {showMcp && <McpView onDone={() => setShowMcp(false)} />}
+    //       {showAccount && <AccountView onDone={() => setShowAccount(false)} />}
+    //       {/* Do not conditionally load ChatView, it's expensive and there's state we don't want to lose (user input, disableInput, askResponse promise, etc.) */}
+    //       <ChatView
+    //         showHistoryView={() => {
+    //           setShowSettings(false)
+    //           setShowMcp(false)
+    //           setShowHistory(true)
+    //         }}
+    //         isHidden={showSettings || showHistory || showMcp || showAccount}
+    //         showAnnouncement={showAnnouncement}
+    //         hideAnnouncement={() => {
+    //           setShowAnnouncement(false)
+    //         }}
+    //       />
+    //     </>
+    //   )}
+    // </>
+  )
+}
+
+const App = () => {
+  return (
+    <ExtensionStateContextProvider>
+      <AppContent />
+    </ExtensionStateContextProvider>
   )
 }
 
